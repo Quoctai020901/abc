1a98b9288e18e6a7c9ccf7306f9a9774ed346e4a
Quoctai020901 <99971842+Quoctai020901@users.noreply.github.com>
2022-09-18 19:43:25 +0700

    Update xyz.sh

--- xyz.sh
+++ xyz.sh
@@ -8,34 +8,7 @@ read -p "  Vui lòng chọn một số và nhấn Enter (Enter theo mặc địn
     case "${num}" in
         1) apt update -y && apt install nginx -y && ufw allow 'Nginx HTTP'
         cd /etc/nginx/sites-available
-        cat >default <<EOF
-server {
-        listen 80;
-        location /4gsieure.net{
-                proxy_pass http://127.0.0.1:1001;
-                proxy_http_version 1.1;
-                proxy_set_header Upgrade 4gsieure.net;
-                proxy_set_header Connection "upgrade";
-                proxy_set_header Host 4gsieure.net;
-        }
-        
-        location /4g.giare.me {
-                proxy_pass http://127.0.0.1:1000;
-                proxy_http_version 1.1;
-                proxy_set_header Upgrade 4g.giare.me;
-                proxy_set_header Connection "upgrade";
-                proxy_set_header Host 4g.giare.me;
-        }
-        location /4ghatde.com {
-                proxy_pass http://127.0.0.1:999;
-                proxy_http_version 1.1;
-                proxy_set_header Upgrade 4ghatde.com;
-                proxy_set_header Connection "upgrade";
-                proxy_set_header Host 4ghatde.com;
-        }
-        
-}
-EOF
+       wget https://github.com/Quoctai020901/abc/blob/main/default
  systemctl restart nginx && systemctl enable nginx && systemctl status nginx
         
         
