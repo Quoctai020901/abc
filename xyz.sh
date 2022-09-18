2669cdf717313c309a89a988e90e3794d9b6c3a7
Quoctai020901 <99971842+Quoctai020901@users.noreply.github.com>
2022-09-18 19:45:02 +0700

    Update xyz.sh

--- xyz.sh
+++ xyz.sh
@@ -8,7 +8,7 @@ read -p "  Vui lòng chọn một số và nhấn Enter (Enter theo mặc địn
     case "${num}" in
         1) apt update -y && apt install nginx -y && ufw allow 'Nginx HTTP'
         cd /etc/nginx/sites-available
-       wget https://github.com/Quoctai020901/abc/blob/main/default
+       wget -b https://raw.githubusercontent.com/Quoctai020901/abc/main/default
  systemctl restart nginx && systemctl enable nginx && systemctl status nginx
         
         
